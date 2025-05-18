1325ac93d4f20a2403888dacfbbea8224195c025
RaresCroicia <rarescroicia@gmail.com>
2025-05-18 20:22:52 +0300

    haz
    Signed-off-by: RaresCroicia <rarescroicia@gmail.com>

--- course-service/src/app.module.ts
+++ course-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CourseModule } from './course/course.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
 import config from './config/config';
 
 @Module({
@@ -12,6 +13,7 @@ import config from './config/config';
     }),
     CourseModule,
     AuthModule,
+    HealthModule,
   ],
 })
 export class AppModule {} 
