58d46607c5eb3d5c24ddefb293e2e711438d2429
RaresCroicia <rarescroicia@gmail.com>
2025-05-27 17:53:46 +0300

    pls
    Signed-off-by: RaresCroicia <rarescroicia@gmail.com>

--- course-service/src/config/config.ts
+++ course-service/src/config/config.ts
@@ -2,8 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => ({
   port: parseInt(process.env.PORT || '3002', 10),
-  ingestionServiceUrl: process.env.INGESTION_SERVICE_URL || 'http://localhost:3003',
-  authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://localhost:3001',
+  ingestionServiceUrl: process.env.INGESTION_SERVICE_URL || 'http://188.245.104.79/ingestion',
+  authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://188.245.104.79/auth',
   jwt: {
     secret: process.env.JWT_SECRET || 'secret',
     expiresIn: process.env.JWT_EXPIRES_IN || '1d',
